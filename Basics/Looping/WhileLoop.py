0a90caacb1c09ac98ea9afa6219233a2c2318ac1
Guhan Ganesan <bharadhiguhan@ymail.com>
2019-06-30 21:35:53 +0530

    Create WhileLoop.py

---
+++ Basics/Looping/WhileLoop.py
@@ -0,0 +1,30 @@
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
+    if(i==5):
+        continue
+    print("test me")
+    
+"""
+Answer:-
+1
+test me
+2
+test me
+3
+test me
+4
+5
+test me
+6
+test me
+7
+test me
+8
+test me
+9
+test me
+10
+test me
+"""
